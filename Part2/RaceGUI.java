27720cd9b25cc63547097b29d1442cdcd1b26d1f
AndyLiu2796 <andyliuhoming@gmail.com>
2025-04-28 15:45:07 +0800

    deleted irrelevant files

--- Part2/RaceGUI.java
+++ Part2/RaceGUI.java
@@ -2,141 +2,84 @@ package Part2;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
 
 public class RaceGUI {
-    public static void main(String[] args) {
-        // Create a JFrame
+    public static void startRaceGUI() {
+        Race race = new Race(30,0);
+        RacePanel racePanel = new RacePanel(race);
         JFrame frame = new JFrame("Horse Race Simulator");
+        final RaceBet[] currentRaceBet = new RaceBet[1];
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000, 1000);
         frame.setLayout(new BorderLayout());
-        frame.setSize(1000,1000);
+        frame.add(racePanel, BorderLayout.CENTER);
+        frame.add(new JTextField("Statistics"), BorderLayout.SOUTH);
+        // Horse h1 = new Horse("@", "Horse 1", 0.9);
+        // race.addHorse(h1);
+        HorsePanel horsePanel = new HorsePanel(race, racePanel);
 
-        JPanel configPanel = createConfigPanel();
-        JPanel trackPanel = createTrackPanel();
-        JPanel statsPanel = createStatsPanel();
-        JPanel horsePanel = createHorsePanel();
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("Options");
+        JMenuItem openHorsePanelItem = new JMenuItem("Open Horse Panel");
+        JMenuItem openStatisticsItem = new JMenuItem("Open Statistics");
+        JMenuItem openBettingItem = new JMenuItem("Open Betting");
+        menu.add(openHorsePanelItem);
+        menuBar.add(menu);
+        frame.setJMenuBar(menuBar);
+        openHorsePanelItem.addActionListener(e -> {
+            JFrame horseFrame = new JFrame("Horse Panel");
+            horseFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            horseFrame.setSize(400, 400);
+            horseFrame.setLayout(new BorderLayout());
+            horseFrame.add(horsePanel, BorderLayout.CENTER);
+            horseFrame.setVisible(true);
+        });
 
-        frame.add(configPanel, BorderLayout.NORTH);
-        frame.add(trackPanel, BorderLayout.CENTER);
-        frame.add(statsPanel, BorderLayout.SOUTH);
-        frame.add(horsePanel, BorderLayout.EAST);
+        menu.add(openStatisticsItem);
+        openStatisticsItem.addActionListener(e -> {
+            Map<String, PerformanceStats> horseStatsMap = new HashMap<>();
+            for (Horse h : race.returnLanes()) {
+                if (h != null) {
+                    horseStatsMap.put(h.getName(), h.getStats());
+                }
+            }
 
+            StatisticsFrame sframe = new StatisticsFrame(horseStatsMap);
+            sframe.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            sframe.setSize(400, 400);
+            sframe.setVisible(true);
+        });
+
+        menu.add(openBettingItem);
+        openBettingItem.addActionListener(e -> {
+            RaceBet raceBet = new RaceBet(race);
+            currentRaceBet[0] = raceBet;
+            BettingFrame bf = new BettingFrame(raceBet, race);
+            bf.setVisible(true);
+        });
+        TrackPanel trackPanel = new TrackPanel(race, racePanel);
+        frame.add(trackPanel, BorderLayout.NORTH);
+        JButton refreshButton = new JButton("Refresh");
+        refreshButton.addActionListener(e -> {
+            racePanel.UpdateScreen();
+        });
+        // frame.add(refreshButton, BorderLayout.WEST);
+        racePanel.UpdateScreen();
+        JButton startButton = new JButton("Start");
+        startButton.addActionListener(e -> {
+            new Thread(() -> {
+                if (currentRaceBet[0] != null) {
+                    race.startRace(racePanel, currentRaceBet[0]);
+                }
+                else {
+                    race.startRace(racePanel, new RaceBet(race));
+                }
+            }).start();;
+            startButton.setText("Reset");
+        });
+        frame.add(startButton, BorderLayout.SOUTH);
+        // frame.add(refreshButton, BorderLayout.WEST);
         frame.setVisible(true);
     }
-    
-
-    // Create a JPanel for the configuration
-    private static JPanel createConfigPanel() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(5, 2));
-
-        // first row - number of lanes
-        panel.add(new JLabel("Number of lanes: "));
-        JTextField lanesInput = new JTextField();
-        panel.add(lanesInput);
-
-        // second row - length of lanes
-        panel.add(new JLabel("Track Length: "));
-        JTextField trackLengthInput = new JTextField();
-        panel.add(trackLengthInput);
-
-        // third row - Track Shape
-        panel.add(new JLabel("Track Shape: "));
-        JComboBox<String> shapeSelector = new JComboBox<>(new String[]{"Oval", "Figure-Eight", "Circular", "Others TBC"});
-        panel.add(shapeSelector);
-
-        // four row - Track Weather
-        panel.add(new JLabel("Weather & Track Condition: "));
-        JComboBox<String> WeatherSelector = new JComboBox<>(new String[]{"Muddy", "Dry", "Icy", "Rainy"});
-        panel.add(WeatherSelector);
-
-        // firth row - confirm button
-        JButton confirmButton = new JButton("Confirm");
-        confirmButton.addActionListener(e -> {
-
-            int lanes = Integer.parseInt(lanesInput.getText()); // I DONT THINK IT CAN PARSE NON-NUMERICAL STRING INTO INT BUT DO THIS FOR NOW
-            int trackLength = Integer.parseInt(trackLengthInput.getText()); // SAME AS ABOVE
-            String shape = (String) shapeSelector.getSelectedItem();
-            String weather = (String) WeatherSelector.getSelectedItem();
-
-            System.out.println("Lanes: " + lanes);
-            System.out.println("Track Length: " + trackLength);
-            System.out.println("Track Shape: " + shape);
-            System.out.println("Weather: " + weather);
-        });
-        panel.add(confirmButton);
-
-        return panel;
-    }
-
-    //
-    private static JPanel createTrackPanel() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(5, 1)); // Example: 5 lanes
-        panel.setBackground(Color.GREEN);
-        return panel;
-    }
-
-    // 
-    private static JPanel createStatsPanel() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
-
-        JLabel statsLabel = new JLabel("Race Statistics:");
-        JTextArea statsArea = new JTextArea(5, 30);
-        statsArea.setEditable(false);
-
-        panel.add(statsLabel, BorderLayout.NORTH);
-        panel.add(new JScrollPane(statsArea), BorderLayout.CENTER);
-
-        return panel;
-    }
-
-    // horse configuration
-    private static JPanel createHorsePanel() {
-        JPanel horsePanel = new JPanel(new GridLayout(3,2));
-
-        JLabel horseNameLabel = new JLabel("Horse Name");
-        JTextField horseNameField = new JTextField();
-        
-        JLabel horseSymbolLabel = new JLabel("Horse Symbol");
-        JTextField horseSymbolField = new JTextField(1); // prob need to implement document filter to limit to one char only
-
-        JLabel horseBreedLabel = new JLabel("Horse Breed");
-        JComboBox<String> horseBreedSelector = new JComboBox<>(new String[]{"Thoroughbred", "Arabian", "Quarter Horse", "Others TBC"});
-
-        // JLabel 
-
-        horsePanel.add(horseNameLabel);
-        horsePanel.add(horseNameField);
-        horsePanel.add(horseSymbolLabel);
-        horsePanel.add(horseSymbolField);
-        horsePanel.add(horseBreedLabel);
-        horsePanel.add(horseBreedSelector);
-
-        return horsePanel;
-    }
-
-
-
 }
-
-// JPanel configPanel = new JPanel();
-//         configPanel.setLayout(new GridLayout(0, 2));
-//         configPanel.setBounds(10, 10, 300, 200);
-        
-//         JLabel horseNameLabel = new JLabel("Horse Name:");
-//         JTextField horseNameField = new JTextField();
-//         JLabel horseSymbolLabel = new JLabel("Horse Symbol:");
-//         JTextField horseSymbolField = new JTextField();
-//         JLabel horseConfidenceLabel = new JLabel("Horse Confidence:");
-//         JTextField horseConfidenceField = new JTextField();
-        
-//         configPanel.add(horseNameLabel);
-//         configPanel.add(horseNameField);
-//         configPanel.add(horseSymbolLabel);
-//         configPanel.add(horseSymbolField);
-//         configPanel.add(horseConfidenceLabel);
-//         configPanel.add(horseConfidenceField);
-        
-//         return configPanel;
